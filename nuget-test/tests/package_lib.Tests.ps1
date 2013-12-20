3df67b97732c807100a93c489a20aef6b2998b3b
hudson <hudson-watchers@newrelic.com>
2013-12-19 19:16:45 -0800

    Updates Agent API NuGet package to version 2.17.268.0

--- nuget-test/tests/package_lib.Tests.ps1
+++ nuget-test/tests/package_lib.Tests.ps1
@@ -12,7 +12,7 @@ Checking to see if a lib file is a particular architecture
 $modulesPath = (Split-Path -parent $MyInvocation.MyCommand.Path).Replace("\tests", "\modules\")
 Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
-	$version = "2.17.266.0"
+	$version = "2.17.268.0"
     Describe "package_lib" {	
 		It "checks to see if lib\NewRelic.Api.Agent.dll exists" {
 			(Test-Path $PackageRoot\lib\NewRelic.Api.Agent.dll) | Should Be $true
