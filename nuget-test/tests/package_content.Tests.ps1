fa9a170b10f7b404ce0211d5da7c3923c2f736f5
hudson <hudson-watchers@newrelic.com>
2014-08-15 15:04:40 -0700

    Updates Agent API NuGet package to version 3.5.107.0

--- nuget-test/tests/package_content.Tests.ps1
+++ nuget-test/tests/package_content.Tests.ps1
@@ -14,7 +14,7 @@ Validating the data in the .nuspec file
 $modulesPath = (Split-Path -parent $MyInvocation.MyCommand.Path).Replace("\tests", "\modules\")
 Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
-	$version = "3.4.24.0"
+	$version = "3.5.107.0"
     Describe "package_content" {
 		
 		Context "When .nuspec file exists and has a metadata element" {
