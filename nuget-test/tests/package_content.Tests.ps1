427bd1b5a3551f56b9c67464a56da60748d2d0ff
hudson <hudson-watchers@newrelic.com>
2015-03-11 13:56:14 -0700

    Updates NewRelic.Agent.Api to version 4.0.143.0

--- nuget-test/tests/package_content.Tests.ps1
+++ nuget-test/tests/package_content.Tests.ps1
@@ -14,7 +14,7 @@ Validating the data in the .nuspec file
 $modulesPath = (Split-Path -parent $MyInvocation.MyCommand.Path).Replace("\tests", "\modules\")
 Import-Module (Join-Path $modulesPath global_config.psm1) -Force
 
-	$version = "3.12.140.0"
+	$version = "4.0.143.0"
     Describe "package_content" {
 		
 		Context "When .nuspec file exists and has a metadata element" {
